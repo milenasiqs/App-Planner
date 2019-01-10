fba7dd07e0824bcb18212420f4d047200b46fb9f
milenasiqs <milena.ifpe@gmail.com>
2019-01-09 23:49:22 -0300

    modelo de página inicial criado

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, MenuController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+ 
+  
+  constructor(public navCtrl: NavController, menu: MenuController) {
+    menu.enable(true);
   }
 
 }
